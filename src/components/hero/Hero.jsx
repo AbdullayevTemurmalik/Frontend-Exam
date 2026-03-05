b27880ccb8099c0e2ff3b616b61c5125a21f8189
TemurMalik <tmaq77@gmail.com>
2026-03-05 09:10:59 +0500

    Edit Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Autoplay, Pagination } from "swiper/modules";
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, ArrowRight, Apple } from "lucide-react";
 import HeroBanner from "../../assets/Heroimg.png";
 
 const Hero = () => {
@@ -18,6 +18,27 @@ const Hero = () => {
     { name: "Groceries & Pets", hasSub: false },
     { name: "Health & Beauty", hasSub: false },
   ];
+
+  const slideContent = (
+    <div className="slide-inner">
+      <div className="slide-text-content">
+        <div className="apple-brand">
+          <Apple size={40} fill="white" />
+          <span>iPhone 14 Series</span>
+        </div>
+        <h2>
+          Up to 10% <br /> off Voucher
+        </h2>
+        <a href="#" className="shop-now-link">
+          <span>Shop Now</span>
+          <ArrowRight size={20} />
+        </a>
+      </div>
+      <div className="slide-image-content">
+        <img src={HeroBanner} alt="iPhone 14" />
+      </div>
+    </div>
+  );
 
   return (
     <section className="container hero-wrap">
@@ -40,20 +61,14 @@ const Hero = () => {
         <Swiper
           spaceBetween={0}
           centeredSlides={true}
-          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          autoplay={{ delay: 3500, disableOnInteraction: false }}
           pagination={{ clickable: true }}
           modules={[Autoplay, Pagination]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <img src={HeroBanner} alt="banner" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={HeroBanner} alt="banner" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={HeroBanner} alt="banner" />
-          </SwiperSlide>
+          {[...Array(5)].map((_, i) => (
+            <SwiperSlide key={i}>{slideContent}</SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </section>
